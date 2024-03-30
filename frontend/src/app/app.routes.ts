dfba3bcffac5ccfa76204188c6466a68379adfc5
Aikarubi <nerupe@floridauniversitaria.es>
2024-03-30 19:02:05 +0100

    Estructura detalle básica

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CatalogoComponent } from './views/catalogo/catalogo.component';
 import { PlanEditorialComponent } from './views/plan-editorial/plan-editorial.component';
 import { SobreNosotrosComponent } from './views/sobre-nosotros/sobre-nosotros.component';
 import { PoliticaPrivacidadComponent } from './views/politica-privacidad/politica-privacidad.component';
+import { DetalleComponent } from './views/detalle/detalle.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -14,4 +15,5 @@ export const routes: Routes = [
     {path: 'plan-editorial', component: PlanEditorialComponent},
     {path: 'sobre-nosotros', component: SobreNosotrosComponent},
     {path: 'politica-privacidad', component: PoliticaPrivacidadComponent},
+    {path: 'detalle', component: DetalleComponent}
 ];
--- frontend/src/app/views/detalle/detalle.component.ts
+++ frontend/src/app/views/detalle/detalle.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './detalle.component.html',
   styleUrl: './detalle.component.css'
 })
