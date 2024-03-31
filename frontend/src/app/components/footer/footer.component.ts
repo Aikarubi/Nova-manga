590f16b425cc451fbfd459e6569980b886bb95b5
Aikarubi <nerupe@floridauniversitaria.es>
2024-03-31 15:34:15 +0200

    HomePage terminada

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SobreNosotrosComponent } from '../../views/sobre-nosotros/sobre-nosotros.component';
+import { PoliticaPrivacidadComponent } from '../../views/politica-privacidad/politica-privacidad.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, SobreNosotrosComponent, PoliticaPrivacidadComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
