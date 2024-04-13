15242a85e8dcf524bed7a2188167627105c503f9
Aikarubi <nerupe@floridauniversitaria.es>
2024-04-13 13:58:26 +0200

    añadir libros en php

--- backend/my-project/src/Controller/LibrosController.php
+++ backend/my-project/src/Controller/LibrosController.php
@@ -35,4 +35,43 @@ class LibrosController extends AbstractController
 
         return $this->json($data);
     }
+
+    #[Route('/insert/libros', name: 'app_insertLibros', methods: ['POST'])]
+    public function insertLibros(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $data = json_decode($request->getContent(), true);
+
+        $libro = new Libros();
+        $libro->setIsbn($data['isbn']);
+        $libro->setNombre($data['nombre']);
+        $libro->setPrecio($data['precio']);
+        $libro->setDescripcion($data['descripcion']);
+        $libro->setTamanyo($data['tamanyo']);
+        $libro->setPaginas($data['paginas']);
+        $libro->setPortada($data['portada']);
+        $libro->setFechaVenta(new \DateTime($data['fecha_venta']));
+        $libro->setEditorial($data['editorial_id']);
+        $libro->setAutor($data['autor_id']);
+
+        $entityManager->persist($libro);
+        $entityManager->flush();
+
+        $responseData = [
+            'isbn' => $libro->getIsbn(),
+            'nombre' => $libro->getNombre(),
+            'precio' => $libro->getPrecio(),
+            'descripcion' => $libro->getDescripcion(),
+            'tamanyo' => $libro->getTamanyo(),
+            'paginas' => $libro->getPaginas(),
+            'portada' => $libro->getPortada(),
+            'fecha_venta' => $libro->getFechaVenta(),
+            'editorial_id' => $libro->getEditorial()->getId(),
+            'autor_id' => $libro->getAutor()->getId()
+
+        ];
+
+        return new JsonResponse($responseData);
+
+    }
 }
