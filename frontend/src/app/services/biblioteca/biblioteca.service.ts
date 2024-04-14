80bbf9ead67dc9084952a6b099ee74edafb21389
Aikarubi <nerupe@floridauniversitaria.es>
2024-04-14 13:05:01 +0200

    servicios añadidos

--- frontend/src/app/services/biblioteca/biblioteca.service.ts
+++ frontend/src/app/services/biblioteca/biblioteca.service.ts
@@ -12,6 +12,7 @@ export class BibliotecaService {
 
     constructor(public http: HttpClient) { }
   
+    //LIBROS
     public getLibros(): Observable<Response[]> {
       const urlGetAllLibros: string = `${this.urlAPI}libros`; 
     
@@ -38,4 +39,60 @@ export class BibliotecaService {
       const urlEliminarLibro: string = `${this.urlAPI}delete/libros/${isbn}`;
       return this.http.delete(urlEliminarLibro);
     }
+
+    //AUTORES
+    public getAutores(): Observable<Response[]> {
+      const urlGetAllAutores: string = `${this.urlAPI}autor`;
+      return this.http.get<Response[]>(urlGetAllAutores);
+    }
+
+    public agregarAutor(autorData: any): Observable<any> {
+      const urlInsertAutor: string = `${this.urlAPI}insert/autores`; 
+    
+      return this.http.post<any>(urlInsertAutor, autorData);
+    }
+
+    public actualizarAutor(id: number, datosAutor: any): Observable<any> {
+      const urlActualizarAutor: string = `${this.urlAPI}update/autores/${id}`; 
+      return this.http.put(urlActualizarAutor, datosAutor);
+    }
+
+    public obtenerAutor(id: number): Observable<any> {
+      const urlObtenerAutor: string = `${this.urlAPI}autores/${id}`;
+      return this.http.get<any>(urlObtenerAutor);
+    }
+
+    public eliminarAutor(id: number): Observable<any> {
+      const urlEliminarAutor: string = `${this.urlAPI}delete/autores/${id}`;
+      return this.http.delete(urlEliminarAutor);
+    }
+
+    //EDITORIALES
+
+    public getEditoriales(): Observable<Response[]> {
+      const urlGetAllEditoriales: string = `${this.urlAPI}editorial`;
+      return this.http.get<Response[]>(urlGetAllEditoriales);
+    }
+
+    public agregarEditorial(editorialData: any): Observable<any> {
+      const urlInsertEditorial: string = `${this.urlAPI}insert/editoriales`; 
+    
+      return this.http.post<any>(urlInsertEditorial, editorialData);
+    }
+
+    public actualizarEditorial(id: number, datosEditorial: any): Observable<any> {
+      const urlActualizarEditorial: string = `${this.urlAPI}update/editoriales/${id}`; 
+      return this.http.put(urlActualizarEditorial, datosEditorial);
+    }
+
+    public obtenerEditorial(id: number): Observable<any> {
+      const urlObtenerEditorial: string = `${this.urlAPI}editoriales/${id}`;
+      return this.http.get<any>(urlObtenerEditorial);
+    }
+
+    public eliminarEditorial(id: number): Observable<any> {
+      const urlEliminarEditorial: string = `${this.urlAPI}delete/editoriales/${id}`;
+      return this.http.delete(urlEliminarEditorial);
+    }
+
 }
