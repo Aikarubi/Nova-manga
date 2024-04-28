06849b0b2dd471357422d87ddbe16c667aa5cbd5
Aikarubi <nerupe@floridauniversitaria.es>
2024-04-28 18:41:55 +0200

    páginado y mejora de imagenes a buena calidad

--- frontend/src/app/views/catalogo/catalogo.component.ts
+++ frontend/src/app/views/catalogo/catalogo.component.ts
@@ -8,7 +8,7 @@ import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [ FormsModule],
+  imports: [FormsModule],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
@@ -28,8 +28,11 @@ export class CatalogoComponent implements OnInit {
     this.getResponse();
   }*/
 
-  terminoBusqueda: string = '';
-  libros: Response[] = [];
+  public terminoBusqueda: string = '';
+  public libros: Response[] = [];
+  public paginaActual: number = 1;
+  public librosPorPagina: number = 9;
+  public paginas: number[] = [];
 
   constructor(private bibliotecaService: BibliotecaService, private router: Router) { }
 
@@ -38,10 +41,26 @@ export class CatalogoComponent implements OnInit {
   }
 
   getLibros(): void {
-    this.bibliotecaService.getLibros()
-      .subscribe(libros => this.libros = libros);
+    this.bibliotecaService.getLibros().subscribe(libros => {
+      this.libros = libros;
+      this.calcularPaginas();
+    });
   }
 
+  calcularPaginas(): void {
+    const totalPaginas = Math.ceil(this.libros.length / this.librosPorPagina);
+    this.paginas = Array.from({ length: totalPaginas }, (_, index) => index + 1);
+  }
+
+  cambiarPagina(pagina: number): void {
+    this.paginaActual = pagina;
+  }
+
+  getLibrosPaginados(): any[] {
+    const indiceInicial = (this.paginaActual - 1) * this.librosPorPagina;
+    const indiceFinal = Math.min(indiceInicial + this.librosPorPagina, this.libros.length);
+    return this.libros.slice(indiceInicial, indiceFinal);
+  }
 
   public verDetalle(isbn: string): void {
     this.router.navigate(['/detalle', isbn]);
