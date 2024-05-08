034e63c5bc21973192319392572d8f660090081a
aikarubi <nererubiperez@gmail.com>
2024-05-08 16:47:26 +0200

    eliminación de archivos validation.guard

--- frontend/src/app/validation.guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanActivateFn, Router } from '@angular/router';
-import { inject } from '@angular/core';
-
-export const validationGuard: CanActivateFn = (route, state) => {
-return false;
-};
